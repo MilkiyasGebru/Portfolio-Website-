417f1cfc8282101e5925683c02a8e38d0b1437ee
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-23 14:47:49 +0300

    Added a contact me page

---
+++ app/contact/page.jsx
@@ -0,0 +1,27 @@
+"use client"
+
+export default function ContactPage(){
+    return (
+        <div className="container flex flex-col justify-center items-center">
+
+            <p >Hi 👋 , Let's get this conversation started.</p>
+            <p></p>
+            <p></p>
+            <form action="" className="flex flex-col justify-around  gap-4 w-5/6 md:w-3/5 overflow-hidden border rounded-md p-4 bg-blue-100 mt-10 ">
+
+                <p className="font-semibold">Name</p>
+                <div className="flex gap-2 flex-col md:flex-row">
+                    <input type="text" className="border py-2 px-4 rounded-md" placeholder="Your First Name"/>
+                    <input type="text" className="border py-2 px-4 rounded-md" placeholder="Your Last Name"/>
+                </div>
+                <p className="font-semibold">Email</p>
+                <input type="email" className="border py-2 px-4 rounded-md w-1/2" placeholder="Your Email"/>
+                <p className="font-semibold">Message</p>
+                <textarea name="" id="" cols="20" rows="10" className="border rounded-md p-4  outline-none resize-none" placeholder="Hi There" value="" onChange={()=>console.log("HI")}></textarea>
+                <button className=" border rounded-md bg-white w-1/6 px-3 py-2 font-semibold">
+                    Send
+                </button>
+            </form>
+        </div>
+    )
+}
