e42169461841e40af61ae33696a52bd71904d475
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-23 15:37:45 +0300

    Fixed issues in the Project Component and fixed the height

--- app/ui/project.jsx
+++ app/ui/project.jsx
@@ -2,19 +2,23 @@ import Image from "next/image";
 export default function ProjectDetail(props) {
     const {project} = props;
     return (
-        <div className="border border-gray-300 rounded-2xl overflow-hidden p-2 w-1/3">
-            <Image
-                src={"/" + project.logo}
-                alt="Project Image"
-                width={300}
-                height={50}
-            />
+        <div className="flex flex-col h-80 justify-between items-center border border-gray-300 rounded-2xl overflow-hidden py-4 px-4 w-4/5 lg:w-1/3">
+            <div className="max-h-14 overflow-hidden">
+                <Image
+                    src={"/" + project.logo}
+                    alt="Project Image"
+                    width={200}
+                    height={150}
+                    className="p-4"
+                />
+            </div>
 
-            <p className="my-3 text-center font-semibold">{project.title}</p>
 
-            <p className="mb-3">{project.description}</p>
+            <p className="my-3 text-center font-semibold text-2xl">{project.title}</p>
 
-            <a href={project.link} className="underline font-semibold mt-2">Github Link</a>
+            <p className="mb-3 font-light text-sm">{project.description}</p>
+
+            <a href={project.link} className="underline font-semibold mt-2 self-start">Github Link</a>
 
         </div>
     )
