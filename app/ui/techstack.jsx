beb33acaec860ae58fa1ae4e9191a76e19323621
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-23 13:26:30 +0300

    added roundedness to the tech stack buttons

--- app/ui/techstack.jsx
+++ app/ui/techstack.jsx
@@ -2,6 +2,6 @@ export default function TechStack(props) {
 
     const {tech} = props;
     return (
-        <div className="bg-gray-100  p-1 m-1 text-sm font-light">{tech}</div>
+        <div className="bg-gray-100  py-2 px-3 rounded-md m-1 text-sm font-light">{tech}</div>
     )
 }
