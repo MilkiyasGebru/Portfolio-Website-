ca57d9ecf4685c65ee27f8926916a373caf76210
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-23 19:54:19 +0300

    Fixed the Tech Stack Positioning on small screens

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import TechStack from "@/app/ui/techstack";
 import ExperienceDetails from "@/app/ui/experience";
 import ProjectDetail from "@/app/ui/project";
+import Link from "next/link";
 
 export default function Home() {
 
@@ -34,14 +35,25 @@ export default function Home() {
             description:"Mekdim is a comprehensive software enterprise project designed to create a feature-rich school portal, encompassing functionalities such as student grade tracking, course registration, and other essential administrative and academic tools.",
             logo:"mekdim.png",
             link:"https://github.com/NathanZK/Mekdim",
-            website:""
+            website:"",
+            presentation_link:""
         },
         {
             title: "Simple Bookmark Manager",
             description:"This project showcases a beautifully crafted, responsive landing page designed to promote a browser bookmark manager extension. It prioritizes a clean, modern aesthetic and user-friendly interface to effectively communicate the extension's value. The design seamlessly adapts to various screen sizes, ensuring an optimal viewing experience across desktops, tablets, and mobile devices.\n",
             logo:"logo-bookmark.svg",
             link:"https://github.com/MilkiyasGebru/Landing-Page",
-            website:"https://milkiyasgebru.github.io/Landing-Page/"
+            website:"https://milkiyasgebru.github.io/Landing-Page/",
+            presentation_link:""
+        },
+        {
+            title: "Benchmark Suite For Explainable AI",
+            description: "Conducted a 3 month long research project focusing on finding different benchmark suits for comparing different explainable AI models under mentorship of Prof. Valeria Bertacco and P.H.D student Andrew McCrab.",
+            logo:"michigan.png",
+            link:"https://github.com/AURA2022-Valeria/BeXAI",
+            presentation_link:"https://www.youtube.com/watch?v=RRyJ8oesyJU",
+            website:""
+
         }
     ];
 
@@ -52,7 +64,7 @@ export default function Home() {
 
       <div className="w-5/6 m-auto">
 
-          <div className="flex flex-col-reverse items-center md:flex-row">
+          <div className="flex flex-col-reverse items-center md:flex-row mb-8">
               <div className="w-4/5">
                 <p className="text-4xl mb-3.5">About Me</p>
                 <p className="text-4xl mb-3.5">I ❤️ to build things</p>
@@ -78,13 +90,13 @@ export default function Home() {
            </div>
 
 
-          <a href="/MILKIYAS-GEBRU-RESUME.pdf" className="underline font-medium" download="cv.pdf">Download Resume</a>
+          <Link href="/MILKIYAS-GEBRU-RESUME.pdf" className="font-bold py-2 px-3 border rounded-md my-32 bg-black text-white" download="cv.pdf">Download Resume</Link>
 
 
 
           <div className="flex gap-8 mt-8 w-full flex-col-reverse lg:flex-row">
 
-              <div className="w-2/3 flex-wrap">
+              <div className="w-full lg:w-2/3 flex-wrap">
 
                   <p className="text-xl font-bold">Stack</p>
 
