f2645c05e92531f55056b63608e92944b70c63b8
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-23 14:41:59 +0300

    Linked the contact button with its page

--- app/ui/navbar.jsx
+++ app/ui/navbar.jsx
@@ -1,17 +1,20 @@
+import Link from "next/link";
+
 export default function Navbar(){
 
     return (
 
-        <div className="flex justify-between w-5/6 m-auto">
-            <div className="border border-gray-200 rounded-md mt-2 p-2 hover:cursor-pointer hover:font-bold">
+        <div className="flex justify-between w-5/6 m-auto p-2">
+            <div className="border border-gray-200 rounded-md  p-2 hover:cursor-pointer hover:font-bold">
                 Home
             </div>
-            <div className="flex  ">
-                <div className="m-2 p-2 border border-gray-200 rounded-md hover:cursor-pointer hover:font-bold ">
+            <div className="flex gap-2 ">
+                <div className=" p-2 border border-gray-200 rounded-md hover:cursor-pointer hover:font-bold ">
                     Projects
                 </div>
-                <div className="m-2 p-2 border border-gray-200 rounded-md hover:cursor-pointer hover:font-bold ">
-                    Contact
+                <div className=" p-2 border border-gray-200 rounded-md hover:cursor-pointer hover:font-bold ">
+                    <Link href="/contact">Contact</Link>
+
                 </div>
 
             </div>
