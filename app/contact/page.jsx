1fbf67eec813db4a226ae6108701fd348134914d
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-23 15:36:48 +0300

    Installed Font Awesome and Added a send icon

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,9 @@
 "use client"
 
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+
 export default function ContactPage(){
     return (
         <div className="container flex flex-col justify-center items-center">
@@ -7,7 +11,7 @@ export default function ContactPage(){
             <p >Hi 👋 , Let's get this conversation started.</p>
             <p></p>
             <p></p>
-            <form action="" className="flex flex-col justify-around  gap-4 w-5/6 md:w-3/5 overflow-hidden border rounded-md p-4 bg-blue-100 mt-10 ">
+            <form action="" className="flex flex-col justify-around  gap-4 w-5/6 md:w-3/5 overflow-hidden border rounded-md p-4 bg-gray-50 mt-10 ">
 
                 <p className="font-semibold">Name</p>
                 <div className="flex gap-2 flex-col md:flex-row">
@@ -19,9 +23,14 @@ export default function ContactPage(){
                 <p className="font-semibold">Message</p>
                 <textarea name="" id="" cols="20" rows="10" className="border rounded-md p-4  outline-none resize-none" placeholder="Hi There" value="" onChange={()=>console.log("HI")}></textarea>
                 <button className=" border rounded-md bg-white w-1/6 px-3 py-2 font-semibold">
-                    Send
+                    <div className="flex gap-4 items-center">
+                        <p>Send</p>
+                        <FontAwesomeIcon icon={faPaperPlane} />
+                    </div>
+
                 </button>
             </form>
         </div>
     )
 }
+
