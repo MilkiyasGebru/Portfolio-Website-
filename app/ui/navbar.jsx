d44e7219c9d2529f6e51e4aed4ad66fbbc7df6f3
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-23 19:49:47 +0300

    Made the navbar sticky

--- app/ui/navbar.jsx
+++ app/ui/navbar.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Navbar(){
 
     return (
-
-        <div className="flex justify-between w-5/6 m-auto p-2">
+        <div className="bg-gray-100 sticky top-0">
+            <div className="flex justify-between w-5/6 m-auto p-2">
             <div className="border border-gray-200 rounded-md  p-2 hover:cursor-pointer hover:font-bold">
                 <Link href="/">
                     Home
@@ -21,6 +21,7 @@ export default function Navbar(){
                 </div>
 
             </div>
+        </div>
         </div>
     )
 
