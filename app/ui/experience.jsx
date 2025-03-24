9f720a9ad4dece6fb8647a2931c48170266bb8bb
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-24 22:29:22 +0300

    Added Technologies used images for experience section

--- app/ui/experience.jsx
+++ app/ui/experience.jsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-
 export default function  ExperienceDetails(props) {
     const {experience} = props;
     console.log("This is the ",experience.logo);
@@ -8,7 +7,7 @@ export default function  ExperienceDetails(props) {
             <div className="flex gap-2">
                 <Image
                     className="border rounded-lg"
-                    src={ "/"+experience.logo}
+                    src={"/" + experience.logo}
                     alt="Experience"
                     width={50}
                     height={50}
@@ -24,6 +23,14 @@ export default function  ExperienceDetails(props) {
             </div>
             <p className="mt-2">{experience.date + "   .   " + experience.duration}</p>
             <p className="font-light mt-2">{experience.description}</p>
+            {experience.images.length > 0 && <div className="flex mt-2 items-center">
+                <p className="font-semibold">Technologies Used:</p>
+                <div className="flex p-2 gap-4 items-center">
+                    {experience.images.map((image, index) => (
+                        <Image src={image} alt="" width={50} height={50} key={index} className="w-12 h-12 object-contain" />
+                    ))}
+                </div>
+            </div>}
         </div>
     )
 }
